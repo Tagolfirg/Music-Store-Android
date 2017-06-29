8e3d82af94656a5075af9231e7d473e420254b04
Klicker <evan.klicker@nisc.coop>
2017-06-29 16:08:35 -0500

    SI-0008 Create Album Track Detail View
       - Remove weird time formatting

--- app/src/main/java/coop/nisc/intern2016/ui/TrackDetailViewController.java
+++ app/src/main/java/coop/nisc/intern2016/ui/TrackDetailViewController.java
@@ -15,11 +15,9 @@ import java.util.concurrent.TimeUnit;
 
 final class TrackDetailViewController {
 
-    private final Context context;
-
     TrackDetailViewController(@NonNull View view,
                               @NonNull Track track) {
-        context = view.getContext();
+        final Context context = view.getContext();
 
         ((TextView) view.findViewById(R.id.track_details_track_name))
                 .setText(track.trackName);
@@ -51,38 +49,9 @@ final class TrackDetailViewController {
 
     @NonNull
     private String getFormattedTrackDuration(int trackTimeMillis) {
-        String implicitFormat =
-                (trackTimeMillis < TimeUnit.HOURS.toMillis(1) ?
-                 new SimpleDateFormat("m:s", Locale.getDefault()).format(new Date(trackTimeMillis)) :
-                 new SimpleDateFormat("H:m:s", Locale.getDefault()).format(new Date(trackTimeMillis)));
-        String[] splitTime = implicitFormat.split(":");
-        int largerTimeUnit = Integer.valueOf(splitTime[0]);
-        int smallerTimeUnit = Integer.valueOf(splitTime[1]);
-
-        int resourceId = determineResourceId(splitTime);
-        if (resourceId != 0) {
-            return context.getResources()
-                    .getQuantityString(resourceId, smallerTimeUnit, largerTimeUnit, smallerTimeUnit);
-        }
-        return implicitFormat;
-    }
-
-    private int determineResourceId(@NonNull String[] splitTime) {
-        int largerTimeUnit = Integer.valueOf(splitTime[0]);
-        if (splitTime.length > 2) {
-            if (largerTimeUnit == 1) {
-                return R.plurals.hour_and_minutes;
-            } else {
-                return R.plurals.hours_and_minutes;
-            }
-        } else if (splitTime.length == 2) {
-            if (largerTimeUnit == 1) {
-                return R.plurals.minute_and_seconds;
-            } else {
-                return R.plurals.minutes_and_seconds;
-            }
-        }
-        return 0;
+        return (trackTimeMillis < TimeUnit.HOURS.toMillis(1) ?
+                new SimpleDateFormat("m:ss", Locale.getDefault()).format(new Date(trackTimeMillis)) :
+                new SimpleDateFormat("H:mm:ss", Locale.getDefault()).format(new Date(trackTimeMillis)));
     }
 
 }
